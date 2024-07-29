097bb384d1b0a040d20852c6a94be2458e37fbd4
Rayhan Baloch <rehan3517@outlook.com>
2024-07-29 20:53:27 +0500

    Added a basic model and templates

---
+++ WPFCRUDFormsGeneratorApp/CrudFormGeneratorConsole/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudFormGeneratorConsole.Models;
+
+public class PersonModel
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Display(Name = "Full Name")]
+    public string PersonName { get; set; } = string.Empty;
+
+    [Display(Name = "Age")]
+    public int Age { get; set; }
+}
